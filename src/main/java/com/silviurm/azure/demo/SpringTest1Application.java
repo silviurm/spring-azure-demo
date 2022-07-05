efca83a16778a1f9b37dedbfc8591bf1271fa262
silviurm <privat3_kill3r@yahoo.com>
2022-07-05 14:16:50 +0300

    initial commit

---
+++ src/main/java/com/silviurm/azure/demo/SpringTest1Application.java
@@ -0,0 +1,21 @@
+package com.silviurm.azure.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringTest1Application {
+
+	@GetMapping("/message")
+	public String message(){
+		return "Hello World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTest1Application.class, args);
+	}
+
+}
